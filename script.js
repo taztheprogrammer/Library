224fea25df306fffc667f887caa89f99c0982b9a
taztheprogrammer <123741432+taztheprogrammer@users.noreply.github.com>
2023-08-23 12:10:53 -0400

    Can submit cardForm
    Creates a card. Full functionality installed

--- script.js
+++ script.js
@@ -4,20 +4,54 @@ const root = document.getElementById('root')
 const cardForm = document.getElementById('cardForm')
 const submit = document.getElementById('submit')
 const close = document.getElementById('close')
-const valueInput = document.getElementById('valueInput')
+
+const titleInput = document.getElementById('titleInput')
+const authorInput = document.getElementById('authorInput')
+const numberInput = document.getElementById('numberInput')
+const radioYes = document.getElementById('radio-yes')
+const radioNo = document.getElementById('radio-no')
+
 const minusElements = document.getElementsByClassName('minus')
 
 const myLibrary = [];
 
-function Book(value) {
-    this.value = value;
+function Book(title, author, didRead, pageNum) {
+    this.title = title;
+    this.author = author;
+    this.didRead = didRead;
+    this.pageNum = pageNum;
 }
+
 function displayLibrary() {
+
 library.innerHTML = '';
+
 myLibrary.forEach(book => {
     const card = document.createElement('div');
     card.classList.add('card');
-    card.innerHTML = book.value
+
+
+    const titleSpan = document.createElement('span');
+    const authorSpan = document.createElement('span');
+    const pagesSpan = document.createElement('span');
+    const readSpan = document.createElement('span');
+
+    titleSpan.id = 'Title';
+    titleSpan.textContent = book.title;
+    card.appendChild(titleSpan);
+
+    authorSpan.id = 'Author';
+    authorSpan.textContent = book.author;
+    card.appendChild(authorSpan);
+
+    pagesSpan.id = 'Pages';
+    pagesSpan.textContent = book.pageNum + " pages";
+    card.appendChild(pagesSpan);
+
+    readSpan.id = 'read';
+    readSpan.textContent = book.didRead ? "Read" : "Not Read";
+    card.appendChild(readSpan);
+
 
     const minus = document.createElement('div');
     minus.classList.add('minus');
@@ -29,22 +63,37 @@ myLibrary.forEach(book => {
 
     minus.addEventListener('click', () => {
         card.remove();
+        myLibrary.pop(card);
     })
 
 });
 }
 
-function addBookToLibrary() {
-    const value = valueInput.value;
-    const newBook = new Book(value);
+submit.addEventListener('click', function(event) {
+    event.preventDefault();
+    
+    let readBoolean
+
+    if (radioYes.checked) {
+        readBoolean = true;
+    } else {
+        readBoolean = false
+    }
+
+    const newBook = new Book(titleInput.value, authorInput.value, readBoolean, numberInput.value)
     myLibrary.push(newBook);
     displayLibrary();
     hidePopUp();
-}
+})
 
 function hidePopUp() {
     cardForm.style.display = 'none';
     library.style.filter = '';
+    titleInput.value = '';
+  authorInput.value = '';
+  numberInput.value = '';
+  radioYes.checked = false;
+  radioNo.checked = false;
 }
 
 
@@ -52,7 +101,8 @@ add.addEventListener('click', () => {
     cardForm.style.display = 'flex'
     library.style.filter = 'blur(5px)'
 })
-submit.addEventListener('click', addBookToLibrary)
+
+
 close.addEventListener('click', hidePopUp)
 
 
